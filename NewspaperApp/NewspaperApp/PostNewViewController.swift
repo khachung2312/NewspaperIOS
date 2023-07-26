82cd3b7811ba2c80399a3994a97f0ce530693faf
qcth9x <120459255+qcth9x@users.noreply.github.com>
2023-07-26 22:14:21 +0700

    màn hình PostNew

---
+++ NewspaperApp/NewspaperApp/PostNewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostNewViewController.swift
+//  NewspaperApp
+//
+//  Created by Lê Đình Linh on 26/07/2023.
+//
+
+import UIKit
+
+class PostNewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
